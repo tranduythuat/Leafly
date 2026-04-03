ad9223d7d1104c9f61e966cbc91fbe59a7e81d3d
tranduythuat <thuattran1810@gmail.com>
2026-04-03 10:12:10 +0700

    Add-Background-image-and-z-index

--- app/features/editor/components/layout/EditorNavbar.vue
+++ app/features/editor/components/layout/EditorNavbar.vue
@@ -2,25 +2,56 @@
   <div class="navbar">
     <button @click="undo">Undo</button>
     <button @click="redo">Redo</button>
+    <button @click="onSave">Save</button>
+    <button @click="onExportHTML">Export HTML</button>
   </div>
 </template>
 
 <script setup>
-import { useEditorStore } from '../../store/editorStore'
-const store = useEditorStore()
+import { useEditorStore } from "../../store/editorStore";
+import { exportTemplate, generateHTML } from "../../utils/template";
 
-const undo = () => store.undo()
-const redo = () => store.redo()
+const store = useEditorStore();
+
+const undo = () => store.undo();
+const redo = () => store.redo();
+
+const onSave = () => {
+  const json = exportTemplate(store);
+
+  console.log("TEMPLATE JSON:", json);
+
+  // 👉 tạm: download file
+  const blob = new Blob([json], { type: "application/json" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "template.json";
+  a.click();
+};
+
+const onExportHTML = () => {
+  const html = generateHTML(store);
+
+  const blob = new Blob([html], { type: "text/html" });
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "template.html";
+  a.click();
+};
 </script>
 
 <style lang="scss">
-  .navbar {
-    display: flex;
-    gap: 10px;
-    button {
-      background: red;
-      display: block;
-      padding: 10px;
-    }
+.navbar {
+  display: flex;
+  gap: 10px;
+  button {
+    background: red;
+    display: block;
+    padding: 10px;
   }
+}
 </style>
